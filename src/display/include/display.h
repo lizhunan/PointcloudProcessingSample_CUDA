7cc501cf97ae09e99ea8afaaf956ce018939e400
zhunan.li <zhunan.li@outlook.com>
2026-03-24 16:50:43 +0800

    update display for neighbor and normals

--- src/display/include/display.h
+++ src/display/include/display.h
@@ -7,6 +7,8 @@
 #include <string>
 #include <thread>
 
+#define POINT_DIM 6
+
 namespace display {
 
 class Display {
@@ -17,7 +19,8 @@ public:
 
 public:
     void set_pointcloud_xyz(const float* points, const int points_num);
-    void set_normal_vector();
+    void set_neighbors(const int* neighbors, const int k, const int point_idx=0);
+    void set_normals(const float* normals, const int points_num);
 
 private:
     void show();
@@ -28,6 +31,9 @@ private:
 
     float* points;
     int points_num;
+    int* neighbors;
+    int neighbors_k;
+    float* normals;
 };
 
 }
--- src/display/src/display.cpp
+++ src/display/src/display.cpp
@@ -11,9 +11,31 @@ Display::Display(const std::string win_name)
 
 void Display::set_pointcloud_xyz(const float* points, const int points_num)
 {	
-	this->points = new float[points_num*3];
-	memcpy(this->points, points,sizeof(float)*points_num*3);
+	this->points = new float[points_num*POINT_DIM];
+	memcpy(this->points, points,sizeof(float)*points_num*POINT_DIM);
 	this->points_num = points_num;
+}
+
+void Display::set_neighbors(const int* neighbors, const int k, const int point_idx)
+{
+	if (this->neighbors != nullptr) {
+        delete[] this->neighbors;
+        this->neighbors = nullptr;
+    }
+
+	this->neighbors = new int[k+1];
+	this->neighbors_k = k;
+	int base_idx = point_idx * k;
+	for (int i=0; i<k+1; i++)
+	{
+		this->neighbors[i] = neighbors[base_idx + i];
+	}
+}
+
+void Display::set_normals(const float* normals, const int points_num)
+{
+	this->normals = new float[points_num*3];
+	memcpy(this->normals, normals,sizeof(float)*points_num*3);
 }
 
 void Display::show()
@@ -37,13 +59,39 @@ void Display::show()
 
 		glPointSize(2);
 		glBegin(GL_POINTS);
-		glColor3f(0,1,0);
+		// glColor3f(0,1,0);
 		for (int i=0; i<points_num; i++)
 		{
-			glVertex3f(points[i*4+0], points[i*4+1], points[i*4+2]);
+			for (int j=0; j<this->neighbors_k; j++)
+			{
+				if (this->neighbors[j] == points[i*POINT_DIM+4]) glColor3f(1,0,0);
+			}
+			
+			glVertex3f(points[i*POINT_DIM+0], points[i*POINT_DIM+1], points[i*POINT_DIM+2]);
+			glColor3f(0,1,0);
 		}
 		glEnd();
-        
+
+		if (normals != nullptr){
+			glLineWidth(1.0f);
+			glColor3f(0,0,1); // blue normals
+			glBegin(GL_LINES);
+			for (int i=0; i<points_num; i++)
+			{
+				float px = points[i*POINT_DIM+0];
+				float py = points[i*POINT_DIM+1];
+				float pz = points[i*POINT_DIM+2];
+				float nx = normals[i*3 + 0];
+				float ny = normals[i*3 + 1];
+				float nz = normals[i*3 + 2];
+				glVertex3f(points[i*POINT_DIM+0], points[i*POINT_DIM+1], points[i*POINT_DIM+2]);
+
+				glVertex3f(px + nx * 10,
+							py + ny * 10,
+							pz + nz * 10);
+			}
+			glEnd();
+		}
 		pangolin::FinishFrame();
     }
     pangolin::DestroyWindow(win_name);
